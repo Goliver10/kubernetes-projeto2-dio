50132e2ae26a160e389698395696e593b51f0daa
Gabriel Oliveira <115432420+Goliver10@users.noreply.github.com>
2022-11-14 19:11:55 -0300

    Add files via upload

---
+++ PROJETO-KUBERNETES 2/script1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Criando a imagem..."
+
+docker build -t goliver10/kubernetes-projeto2-dio/.
+
+echo "Fazendo o push das imagens..."
+
+docker push goliver10/kubernetes-projeto2-dio
+
+echo "Criando previamente os secrets utilizados..."
+
+kubectl apply -f ./secrets.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./app-deployment.yml
+kubectl apply -f ./mysql-deployment.yml
+
+echo "Criando o loadbalancer..."
+
+kubectl apply -f ./load-balancer.yml
+
+echo "Executando o arquivo gitlab..."
+
+kubectl apply -f ./gitlab-ci.yml
